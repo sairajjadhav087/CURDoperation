498de947a10cb56c339b68c708b00abafd4f53d1
Admin <Admin@DESKTOP-A4TGG22>
2026-01-22 08:38:34 +0530

    Add project files.

---
+++ CURDoperation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURDoperation
+{
+    internal class Program
+    {
+        void show()
+        { Console.WriteLine("hello")
+            }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.show();
+        }
+    }
+}
